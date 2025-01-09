166e3ce2dbc3611b298844a04b686e8902f2a6ff
Samuel Zubicaray Perez <zubicaray@users.noreply.github.com>
2025-01-09 22:53:44 +0100

    Update makeService.bat

--- makeService.bat
+++ makeService.bat
@@ -1,3 +1,3 @@
 prunsrv //IS//ClipperVersProdeo --Install="C:\path\to\prunsrv.exe" --Jvm=auto ^
 --StartMode=jvm --StopMode=jvm --Classpath="C:\path\to\HyperFileService.jar" ^
---StartClass=HyperFileService --StartMethod=start --StopClass=MainService --StopMethod=stop
+--StartClass=HyperFileService --StartMethod=start --StopClass=HyperFileService --StopMethod=stop
